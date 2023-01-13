ef3c6d56b2a3d3d32535c08bfcac5e7482bb0282
Juan-45 <quijuan12@gmail.com>
2023-01-12 22:58:20 -0300

    Add baseURL

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='portfolioDeployed'>
       <App />
     </BrowserRouter>
   </StrictMode>
